ecebab98d405039385182b747060b024fd424de5
Fernando Barbosa <fernando_fe1@hotmail.com>
2025-08-06 15:18:13 -0300

    mentoria e curso

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,31 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
 
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso java");
+        curso1.setDescricao("descrição curso java");
+        curso1.setCargaHoraria(8);
 
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso javaScript");
+        curso2.setDescricao("descrição curso javaScript");
+        curso2.setCargaHoraria(18);
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("mentoria java");
+        mentoria.setDescricao("descricao mentoria java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(mentoria);
     }
+
 }
